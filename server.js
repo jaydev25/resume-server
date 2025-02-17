a73ca4bfbcec6240bd23c189300f5dde07384ca3
Jaydev Thomke <jaydevthomke.in@gmail.com>
2025-02-17 11:34:21 +0530

    added github workflows

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { WebSocketServer } = require('ws');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Enable CORS
 app.use(cors());
